62df6ba3de3cc2dde245e8454b0bc143289817d8
ishigaki0302 <i.sisiking@outlook.jp>
2024-05-20 15:33:56 +0900

    デバッグ

--- utils/convert2request.py
+++ utils/convert2request.py
@@ -6,43 +6,44 @@ from globals import *
 def convert2request(input_file, output_file):
     # CSVファイルを読み込み、リストに格納する
     # ---------------------------------------------
-    # data = []
-    # with open(input_file, 'r', encoding='utf-8') as file:
-    #     reader = csv.reader(file)
-    #     for row in reader:
-    #         data.append(row)
+    data = []
+    with open(input_file, 'r', encoding='utf-8') as file:
+        reader = csv.reader(file)
+        next(reader)  # ヘッダ行を読み飛ばす
+        for row in reader:
+            data.append(row)
     # ---------------------------------------------
-    data = KnownsDataset(DATA_DIR)
+    # data = KnownsDataset(DATA_DIR)
     # ---------------------------------------------
     # リクエストの形式に変換する
     request = []
     # ---------------------------------------------
-    # for row in data:
-    #     prompt, subject, attribute = row
-    #     request.append({
-    #         "prompt": prompt,
-    #         "subject": subject,
-    #         "target_new": {"str": ""},
-    #         "target_true": attribute
-    #     })
-    # ---------------------------------------------
-    for item in data:
+    for row in data:
+        prompt, subject, attribute = row
         request.append({
-            "prompt": item["prompt"],
-            "subject": item["subject"],
+            "prompt": prompt,
+            "subject": subject,
             "target_new": {"str": ""},
-            "target_true": item["attribute"]
+            "target_true": attribute
         })
+    # ---------------------------------------------
+    # for item in data:
+    #     request.append({
+    #         "prompt": item["prompt"],
+    #         "subject": item["subject"],
+    #         "target_new": {"str": ""},
+    #         "target_true": item["attribute"]
+    #     })
     # ---------------------------------------------
     # JSON形式で出力する
     with open(output_file, 'w', encoding='utf-8') as file:
         json.dump(request, file, indent=4, ensure_ascii=False)
 
 # 使用例
-# input_file = 'data/text_data_converted_to_csv.csv'
-# output_file = 'data/text_data_converted_to_csv.json'
+input_file = 'data/text_data_converted_to_csv.csv'
+output_file = 'data/text_data_converted_to_csv.json'
 # input_file = 'data/en2jp_data.csv'
 # output_file = 'data/en2jp_data.json'
-input_file = 'data/known_1000.json'
-output_file = 'data/known_1000_convert.json'
+# input_file = 'data/known_1000.json'
+# output_file = 'data/known_1000_convert.json'
 convert2request(input_file, output_file)
